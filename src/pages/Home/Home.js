e3b644cd21e5cf81a575393d57aa6ebb3e70d583
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-12-28 12:36:58 +0600

    navbar done

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className='grid grid-cols-1 lg:grid-cols-3'>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
         </div>
     );
 };
--- src/pages/Navbar/Navbar.js
+++ src/pages/Navbar/Navbar.js
@@ -1,48 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../images/logo.jpg';
 
 const Navbar = () => {
+    const menuItem = 
+    <>
+    <Link><li className='mr-5'>Home</li></Link>
+    <Link><li className='mr-5'>Media</li></Link>
+    <Link><li className='mr-5'>Message</li></Link>
+    <Link><li className='mr-5'>About</li></Link>
+    </>
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-teal-500 text-white sticky">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
-            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li><a>Item 1</a></li>
-              <li tabIndex={0}>
-                <a className="justify-between">
-                  Parent
-                  <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-                </a>
-                <ul className="p-2">
-                  <li><a>Submenu 1</a></li>
-                  <li><a>Submenu 2</a></li>
-                </ul>
-              </li>
-              <li><a>Item 3</a></li>
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-teal-500 text-white rounded-box w-52">
+              {menuItem}
             </ul>
           </div>
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+          <Link className='text-xl font-bold flex items-center'><img src={logo} className='w-10 rounded-full mr-4' alt="" /> Social <span>Networks</span></Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
-            <li><a>Item 1</a></li>
-            <li tabIndex={0}>
-              <a>
-                Parent
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-              </a>
-              <ul className="p-2">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
-            <li><a>Item 3</a></li>
+          {menuItem}
           </ul>
         </div>
         <div className="navbar-end">
-          <a className="btn">Get started</a>
+          <ul className='flex mr-8'>
+          <Link><li className='mr-5'>Login</li></Link>
+          <Link><li>Register</li></Link>
+          </ul>
         </div>
       </div>
     );
