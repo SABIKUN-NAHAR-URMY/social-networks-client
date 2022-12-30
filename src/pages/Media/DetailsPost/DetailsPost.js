199139f8ba9d8965a62c936b11dc59744552e9a6
SABIKUN-NAHAR-URMY <snurmy18@gmail.com>
2022-12-30 20:38:38 +0600

    reaction added done

--- src/pages/Media/DetailsPost/DetailsPost.js
+++ src/pages/Media/DetailsPost/DetailsPost.js
@@ -2,16 +2,20 @@ import React, { useContext, useEffect, useState } from 'react';
 import { toast } from 'react-hot-toast';
 import { Navigate, useLoaderData, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../../Contexts/AuthProvider/AuthProvider';
+import { FaRegHeart } from "react-icons/fa";
 
 const DetailsPost = () => {
     const { user } = useContext(AuthContext);
     const data = useLoaderData();
     console.log(data);
+    
 
     const [update, setUpdate] = useState(false);
 
     const [comments, setComments] = useState([]);
     const [redirect, setRedirect] = useState(false);
+
+    const [count, setCount] = useState(0);
 
     let location = useLocation();
     let from = location.state?.from?.pathname || "/";
@@ -19,13 +23,13 @@ const DetailsPost = () => {
     const handelReactionComment = (event) => {
         event.preventDefault();
         const form = event.target;
-        const rating = form.rating.value;
+        const reaction = form.reaction.value;
         const comment = form.comment.value;
 
         const commentWrite = {
             postId: data._id,
             comment,
-            rating,
+            reaction: reaction,
             commenter: user?.displayName,
             commenterEmail: user?.email,
             commenterImage: user?.photoURL,
@@ -71,8 +75,9 @@ const DetailsPost = () => {
                                 <div className="card-actions ">
 
                                     <form onSubmit={handelReactionComment} className='m-10 block lg:flex w-full'>
+                                    
+                                    <input type="text" name='reaction' value={count} className="input input-bordered w-16 mr-4" /><FaRegHeart className='w-20 h-20 pb-7 pr-3 cursor-pointer' onClick={()=>setCount(count + 1)}></FaRegHeart>
                                         <textarea name='comment' className="textarea textarea-bordered w-full h-5 mr-4" placeholder="Write Comment"></textarea>
-                                        <input type="number" name='rating' placeholder='Rating' className="input input-bordered w-full mr-4" />
 
                                         <input className='btn bg-gradient-to-r from-teal-700 to-teal-400' type="submit" value="Submit Comment" />
                                     </form>
@@ -93,7 +98,7 @@ const DetailsPost = () => {
                                     <div className="block lg:flex justify-between items-center">
                                         
                                         <h2>Comment: {comment.comment}</h2>
-                                        <h2>Rating: {comment.rating}</h2>
+                                        <h2>Reaction: {comment.reaction}</h2>
                                         <div className='flex items-center'>
                                         <img className='w-12 h-12 rounded-full' src={comment.commenterImage} alt="" />
                                         <h2> {comment.commenter}</h2>
